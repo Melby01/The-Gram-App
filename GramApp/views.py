15a28e783b538d7325b9714bafd3c1bbe01de759
Melby01 <melby.okozi@student.moringaschool.com>
2021-07-11 23:48:54 +0300

    Make the welcome.html page

--- GramApp/views.py
+++ GramApp/views.py
@@ -3,8 +3,7 @@ from django.http  import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to The Instagram App')
-
+    return render(request, 'welcome.html')
 def login(request):
     context = {}
     return render(request, 'all-gram//login.html', context)
