b5479c6942c16a75d34d731b67d922ce15fdacc0
Melby01 <melby.okozi@student.moringaschool.com>
2021-07-12 01:04:43 +0300

    Making the DB

--- GramApp/models.py
+++ GramApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length =60)
+    caption = models.TextField(max_length=300,null=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes= models.IntegerField(default=0)
+    dislikes= models.IntegerField(default=0)
+    
+    def save_image(self):
+        self.save()
+        
+    def delete_image(self):
+        self.delete
+        
+    def __str__(self):
+        return self.content
+
+    @property
+    def number_of_comments(self):
+        return Comment.objects.filter(post_connected=self).count()
