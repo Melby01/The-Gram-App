fa7e3e69bd2c48f6f19837218b67d1f32a055344
Melby01 <melby.okozi@student.moringaschool.com>
2021-07-12 00:29:34 +0300

    Include the navbar and the search bar

--- GramApp/url.py
+++ GramApp/url.py
@@ -5,4 +5,5 @@ urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url('^login/',views.login, name = 'login'),
     url('^register/',views.register, name= 'register'),
+    url('^search/',views.search_results, name='search_results')
 ]
--- GramApp/views.py
+++ GramApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def welcome(request):
@@ -10,5 +11,25 @@ def login(request):
     return render(request, 'all-gram//login.html', context)
 
 def register(request):
-    context = {}
+    form = UserCreationForm()
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+    context = {'form':form}
     return render(request, 'all-gram/register.html', context)
+
+def search_results(request):
+    
+    if 'user' in request.GET and request.GET["user"]:
+        search_term = request.GET.get("user")
+        searched_user = Image.search_by_name(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-gram/search.html',{"message":message,"user": searched_user})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-gram/search.html',{"message":message})
