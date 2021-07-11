b93f31932ea88522415dd9b661a4f9a2f048fd73
Melby01 <melby.okozi@student.moringaschool.com>
2021-07-12 02:16:04 +0300

    Initial commit

--- GramApp/admin.py
+++ GramApp/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Image,tags,Comment
+from .models import Image,Profile,tags,Comment
 # Register your models here.
 
 
 admin.site.register(Image)
- 
+admin.site.register(Profile)
 admin.site.register(tags)
 admin.site.register(Comment)
