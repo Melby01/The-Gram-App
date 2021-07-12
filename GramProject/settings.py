f10dfc6efbcea63fcb9e4fec50a593b1ecb0e190
Melby01 <melby.okozi@student.moringaschool.com>
2021-07-12 22:50:35 +0300

    Initial commit

--- GramProject/settings.py
+++ GramProject/settings.py
@@ -50,8 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-
+ 
 ]
 
 ROOT_URLCONF = 'GramProject.urls'
@@ -130,7 +129,7 @@ STATICFILES_DIR = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+ 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
