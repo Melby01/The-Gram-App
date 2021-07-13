f89e6aff2db4454659905feb7e2d4980225dec62
Melby01 <melby.okozi@student.moringaschool.com>
2021-07-13 23:18:19 +0300

    Django registration

--- GramApp/forms.py
+++ GramApp/forms.py
@@ -1,39 +1,31 @@
 from django import forms
-from .models import Image,Profile,Comment
-from django.contrib.auth.forms import User
+from .models import *
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
 
 class NewPostForm(forms.ModelForm):
     class Meta:
         model = Image
-        exclude = ['pub_date', 'Author', 'author_profile']
-        
+        exclude = ['pub_date']
 
-class GramLetterForm(forms.Form):
+class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)
     email = forms.EmailField(label='Email')
-    class Meta:
-        model = Image
-        exclude = ['pub_date', 'Author', 'author_profile']
-        
-        
-class ProfileForm(forms.ModelForm):
+    
+class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        exclude = ['user'] 
+        fields = ['image']
         
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
-    
+
     class Meta:
         model = User
         fields = ['username', 'email']
         
-        
-class NewsLetterForm(forms.Form):
-    your_name = forms.CharField(label='First Name',max_length=30)
-    email = forms.EmailField(label='Email')   
-    
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        exclude = ['author']
+        fields = ('content',)
