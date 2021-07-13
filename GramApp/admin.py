76491fec3afc2488671771b1f5f3fd6df1e9e002
Melby01 <melby.okozi@student.moringaschool.com>
2021-07-13 23:51:51 +0300

    Update the admin authentication

--- GramApp/admin.py
+++ GramApp/admin.py
@@ -1,9 +1,10 @@
+
 from django.contrib import admin
-from .models import Image,Profile,Comment,Preference
+from .models import Image,Profile,tags,Comment
 # Register your models here.
 
 
 admin.site.register(Image)
 admin.site.register(Profile)
+admin.site.register(tags)
 admin.site.register(Comment)
-admin.site.register(Preference)
