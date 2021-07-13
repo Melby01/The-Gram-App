f032e670746b6c4199b380e6d74a79e21ba19bf4
Melby01 <melby.okozi@student.moringaschool.com>
2021-07-13 23:41:50 +0300

    Working on the URL

--- GramApp/url.py
+++ GramApp/url.py
@@ -1,9 +1,17 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url('^login/',views.login, name = 'login'),
-    url('^register/',views.register, name= 'register'),
-    url('^search/',views.search_results, name='search_results')
+    url('^$',views.welcome,name = 'index'),
+    url('^search/',views.search_results, name='search_results'),
+    url('^post/',views.views.views.new_post, name='post'),
+    url('^profile/',views.profile, name='profile'),
+    url('^newpost/',views.newPost, name='newPost'),
+    url('^edit_profile/',views.editProfile, name='update_Profile'),
+    
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
